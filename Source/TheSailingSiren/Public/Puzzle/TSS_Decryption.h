72f5732245c32a75d0dafbc3502dc091ed5863dc
Marcel <54372177+Sihnor@users.noreply.github.com>
2024-06-11 20:44:46 +0200

    Add runes foor decrypt puzzle

---
+++ Source/TheSailingSiren/Public/Puzzle/TSS_Decryption.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TSS_Decryption.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESAILINGSIREN_API UDecryption : public UUserWidget
+{
+	GENERATED_BODY()
+};
